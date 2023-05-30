5dc5e373e2d97deeb90aa63330cfde9658d84224
RemcoSchrijver <remcoschrijv3r@gmail.com>
2023-05-30 13:13:07 +0200

    Added accuracy metric

--- src/app.py
+++ src/app.py
@@ -1,7 +1,5 @@
 import numpy as np
 import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics import confusion_matrix, accuracy_score
 import pickle
 import joblib
 
--- src/train_model.py
+++ src/train_model.py
@@ -1,7 +1,10 @@
+import json
+import os
 import pandas as pd
 import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import confusion_matrix, accuracy_score
 
 dataset = pd.read_csv('a1_RestaurantReviews_HistoricDump.tsv', delimiter='\t', quoting=3)
 
@@ -14,3 +17,18 @@ classifier = GaussianNB()
 classifier.fit(X_train, y_train)
 
 joblib.dump(classifier, 'out/c2_Classifier_Sentiment_model.joblib')
+
+y_pred = classifier.predict(X_test)
+
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+
+ac = accuracy_score(y_test, y_pred)
+summary_file = os.path.join("summary.json")
+with open(summary_file, "w") as fd:
+    json.dump(
+        {
+            "accuracy": ac,
+        },
+        fd
+    )
