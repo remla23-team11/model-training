2a8cda5345cb6d9d84a93904043ec84f6863089a
RemcoSchrijver <remcoschrijv3r@gmail.com>
2023-05-30 14:04:47 +0200

    Added more metrics and explained in the readme

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # model-training
 Repository containing the ML training pipeline
+
+Dependencies can be installed using the requirements.txt, the Python version used to run this project is 3.9.16, if you
+don't have this installed consider using [pyenv](https://github.com/pyenv/pyenv).
+
+## DVC
+This dvc is set up in two different stages, the preprocessing_dataset stage and the train_model stage. 
+
+To fetch the current version of the pipeline one can use: `dvc pull`.
+
+To run the pipeline use `dvc pull` and to force a rune use `dvc pull -f`.
+
+Now you can see the metrics by using `dvc metrics show`.
--- src/train_model.py
+++ src/train_model.py
@@ -4,7 +4,7 @@ import pandas as pd
 import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
-from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.metrics import accuracy_score, roc_curve, auc, precision_score
 
 dataset = pd.read_csv('a1_RestaurantReviews_HistoricDump.tsv', delimiter='\t', quoting=3)
 
@@ -20,15 +20,20 @@ joblib.dump(classifier, 'out/c2_Classifier_Sentiment_model.joblib')
 
 y_pred = classifier.predict(X_test)
 
-cm = confusion_matrix(y_test, y_pred)
-print(cm)
+fpr, tpr, thresholds = roc_curve(y_test, y_pred)
+auc_val = auc(fpr, tpr)
 
 ac = accuracy_score(y_test, y_pred)
+
+pc = precision_score(y_test, y_pred)
+
 summary_file = os.path.join("summary.json")
 with open(summary_file, "w") as fd:
     json.dump(
         {
             "accuracy": ac,
+            "AUC": auc_val,
+            "precision": pc
         },
         fd
     )
