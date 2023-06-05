fff992ecb6abcaf71cf7506c12b9912db8615822
RemcoSchrijver <remcoschrijv3r@gmail.com>
2023-06-05 23:56:00 +0200

    Added linters

--- src/preproccesing_dataset.py
+++ src/preproccesing_dataset.py
@@ -1,20 +1,21 @@
+import pickle
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
-import pickle
 import joblib
 
 from preprocessing import clean_review
 
 dataset = pd.read_csv('a1_RestaurantReviews_HistoricDump.tsv', delimiter='\t', quoting=3)
+reviews = dataset['Review']
 
 corpus = []
-for review in dataset['Review']:
+for review in reviews:
     corpus.append(clean_review(review))
 
 cv = CountVectorizer(max_features=1420)
 preprocessed_data = cv.fit_transform(corpus).toarray()
 
-bow_path = 'out/c1_BoW_Sentiment_Model.pkl'
-pickle.dump(cv, open(bow_path, "wb"))
+BOW_PATH = 'out/c1_BoW_Sentiment_Model.pkl'
+pickle.dump(cv, open(BOW_PATH, "wb"))
 
 joblib.dump(preprocessed_data, 'out/preprocessed.joblib')
