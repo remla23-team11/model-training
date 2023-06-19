26a06955e2556257f789b62c8024d0c879a89b60
RemcoSchrijver <remcoschrijv3r@gmail.com>
2023-06-19 23:08:10 +0200

    Added load data to app.py

--- src/load_data.py
+++ src/load_data.py
@@ -1,5 +1,12 @@
 import gdown
 
-url = 'https://drive.google.com/uc?id=1hIRNz5-op9WER1VgOolx9lQD26Z82925'
-output = 'out/dataset.tsv'
-gdown.download(url, output, quiet=False)
+
+def load_data():
+    url = 'https://drive.google.com/uc?id=1hIRNz5-op9WER1VgOolx9lQD26Z82925'
+    output = 'out/dataset.tsv'
+    gdown.download(url, output, quiet=False)
+
+
+if __name__ == '__main__':
+    load_data()
+
